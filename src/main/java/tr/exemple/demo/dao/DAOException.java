2550bd90ee74b81d6ea0791bb05af6e5f2c66867
thra <thierry.raimond@wanadoo.fr>
2017-12-09 17:35:44 +0100

    ajout javadoc à la classe DAOException.java

--- src/main/java/tr/exemple/demo/dao/DAOException.java
+++ src/main/java/tr/exemple/demo/dao/DAOException.java
@@ -1,18 +1,22 @@
 package tr.exemple.demo.dao;
 
+/**
+ * Classe qui gère les exceptions liées au DAO
+ *
+ */
 public class DAOException extends RuntimeException {
     /*
      * Constructeurs
      */
-    public DAOException( String message ) {
-        super( message );
+    public DAOException(String message) {
+        super(message);
     }
 
-    public DAOException( String message, Throwable cause ) {
-        super( message, cause );
+    public DAOException(String message, Throwable cause) {
+        super(message, cause);
     }
 
-    public DAOException( Throwable cause ) {
-        super( cause );
+    public DAOException(Throwable cause) {
+        super(cause);
     }
 }
