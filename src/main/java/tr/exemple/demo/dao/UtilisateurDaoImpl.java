c6a3d104a238b41ef41b6f8a4db784c63ee9fdeb
thra <thierry.raimond@wanadoo.fr>
2018-01-01 11:39:20 +0100

    requete SQL linux respecte la casse

--- src/main/java/tr/exemple/demo/dao/UtilisateurDaoImpl.java
+++ src/main/java/tr/exemple/demo/dao/UtilisateurDaoImpl.java
@@ -20,8 +20,8 @@ import tr.exemple.demo.beans.Utilisateur;
 public class UtilisateurDaoImpl implements UtilisateurDao {
 
     private DAOFactory daoFactory;
-    private static final String SQL_SELECT_PAR_EMAIL = "SELECT id, email, nom, mot_de_passe, date_inscription FROM Utilisateur WHERE email = ?";
-    private static final String SQL_INSERT = "INSERT INTO Utilisateur (email, mot_de_passe, nom, date_inscription) VALUES (?, ?, ?, NOW())";
+    private static final String SQL_SELECT_PAR_EMAIL = "SELECT id, email, nom, mot_de_passe, date_inscription FROM utilisateur WHERE email = ?";
+    private static final String SQL_INSERT = "INSERT INTO utilisateur (email, mot_de_passe, nom, date_inscription) VALUES (?, ?, ?, NOW())";
 
     /**
      * Constructeur
